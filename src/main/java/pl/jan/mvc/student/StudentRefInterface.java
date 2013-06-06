a2b2966601a53f0b8e7f82b78b5d76151625b208
Jan Kowalski <jan.kowalski@allegro.pl>
2013-06-06 11:53:27 +0200

    Initial commit

---
+++ src/main/java/pl/jan/mvc/student/StudentRefInterface.java
@@ -0,0 +1,6 @@
+package pl.jan.mvc.student;
+
+public interface StudentRefInterface {
+  public void refresh(String firstName, String lastName, String course,
+                      String address);
+}
